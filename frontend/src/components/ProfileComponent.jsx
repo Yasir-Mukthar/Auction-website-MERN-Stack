8e2588e96fd82f18bfbcad8ac0742d1d74689475
hassaan <mrhassaan929@gmail.com>
2024-07-20 19:50:46 +0500

    Removed profile section

--- frontend/src/components/ProfileComponent.jsx
+++ frontend/src/components/ProfileComponent.jsx
@@ -20,44 +20,8 @@ const ProfileComponent = () => {
 
   return (
     <>
-      <div className="flex flex-col gap-4 w-full">
-        <div className="px-7 py-4 w-full bg-theme-bg text-white rounded-2xl">
-          <h2 className=" font-bold text-xl border-b border-border-info-color pb-3 mb-5 ">
-            Profile Summary
-          </h2>
-          {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-            <div className="flex items-center justify-between p-4 rounded-lg border border-border-info-color hover:border-theme-color transition-all">
-              <div>
-                <p className="text-theme-color font-bold text-lg">45.36k</p>
-                <p className="text-body-text-color">Followers</p>
-              </div>
-              <FaUserPlus size={25} />
-            </div>
-            <div className="flex items-center justify-between p-4 rounded-lg border border-border-info-color hover:border-theme-color transition-all">
-              <div>
-                <p className="text-theme-color font-bold text-lg">15.36k</p>
-                <p className="text-body-text-color">Followed</p>
-              </div>
-              <FaUserPlus size={25} />
-            </div>
-            <div className="flex items-center justify-between p-4 rounded-lg border border-border-info-color hover:border-theme-color transition-all">
-              <div>
-                <p className="text-theme-color font-bold text-lg">5.18k</p>
-                <p className="text-body-text-color">Liked</p>
-              </div>
-
-              <AiFillLike size={25} />
-            </div>
-            <div className="flex items-center justify-between p-4 rounded-lg border border-border-info-color hover:border-theme-color transition-all">
-              <div>
-                <p className="text-theme-color font-bold text-lg">45.25 ETH</p>
-                <p className="text-body-text-color">Balance</p>
-              </div>
-              <BsCurrencyExchange size={25} />
-            </div>
-          </div> */}
-        </div>
-        <div className="flex flex-wrap gap-4 lg:flex-nowrap">
+      <div className="flex flex-col gap-4 w-full">        
+        <div className="flex min-h-[400px] flex-wrap gap-4 lg:flex-nowrap">
           <div className="px-7 py-4 w-full bg-theme-bg text-white rounded-2xl">
             <div className="font-bold flex justify-between items-center border-b border-border-info-color pb-3 mb-5 ">
               <h2 className="text-xl ">Personal Info</h2>
@@ -68,7 +32,7 @@ const ProfileComponent = () => {
                 <FaRegEdit size={16} /> <span>Edit</span>
               </Link>
             </div>
-            <ul className="flex flex-col gap-2 font-medium text-body-text-color">
+            <ul className="flex flex-col gap-3 font-medium text-body-text-color">
               <li>
                 Name:{" "}
                 <span className="float-right font-normal">
